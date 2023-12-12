8d133603450e0d1aa8240fab80e8500765f39485
Mamba-cita <EDWINSJOHN52@gmail.com>
2023-12-12 08:59:17 +0300

    fix betty

--- vars.c
+++ vars.c
@@ -7,29 +7,33 @@
  * @buf: the buffer for the character.
  * @p: position of the current address in the buffer.
  *
- * Return: 1( if chain delimiter), otherwise 0.
+ * Return: 1( if chain delimeter), otherwise 0.
  */
 int isa_chain(info_t *info, char *buf, size_t *p)
 {
-    size_t index = *p;
+	size_t index = *p;
 
-    if (buf[index] == '|' && buf[index + 1] == '|') {
-        buf[index] = 0;
-        index++;
-        info->cmd_buf_type = CMD_OR;
-    } else if (buf[index] == '&' && buf[index + 1] == '&') {
-        buf[index] = 0;
-        index++;
-        info->cmd_buf_type = CMD_AND;
-    } else if (buf[index] == ';') {
-        buf[index] = 0;
-        info->cmd_buf_type = CMD_CHAIN;
-    } else {
-        return 0;
-    }
-
-    *p = index;
-    return 1;
+	if (buf[index] == '|' && buf[index + 1] == '|')
+	{
+		buf[index] = 0;
+		index++;
+		info->cmd_buf_type = CMD_OR;
+	}
+	else if (buf[index] == '&' && buf[index + 1] == '&')
+	{
+		buf[index] = 0;
+		index++;
+		info->cmd_buf_type = CMD_AND;
+	}
+	else if (buf[index] == ';') 
+	{
+		buf[index] = 0; 
+		info->cmd_buf_type = CMD_CHIAN;
+	}
+	else
+		return (0);
+	*p = index;
+	return (1);
 }
 
 /**
@@ -37,27 +41,33 @@ int isa_chain(info_t *info, char *buf, size_t *p)
  * @info: the structure parameter.
  * @buf: the buffer for character.
  * @p: position of the current address in the buffer.
- * @index: starting index in the buffer
+ * @i: starting index in the buffer
  * @len: length of the buffer
  *
  * Return: (void)
  */
 void find_chain(info_t *info, char *buf, size_t *p, size_t index, size_t len)
 {
-    if (info->cmd_buf_type == CMD_AND) {
-        if (info->status) {
-            buf[index] = 0;
-            index = len;
-        }
-    }
-    if (info->cmd_buf_type == CMD_OR) {
-        if (!info->status) {
-            buf[index] = 0;
-            index = len;
-        }
-    }
+	size_t index = *p;
 
-    *p = index;
+	if (info->cmd_buf_type == CMD_AND)
+	{
+		if (info->status)
+		{
+			buf[i] = 0;
+			index = len;
+		}
+	}
+	if (info->cmd_buf_type == CMD_OR)
+	{
+		if (!info->status)
+		{
+			buf[i] = 0;
+			index = len;
+		}
+	}
+
+	*p = index;
 }
 
 /**
@@ -69,28 +79,28 @@ void find_chain(info_t *info, char *buf, size_t *p, size_t index, size_t len)
  */
 int restore_alias(info_t *info)
 {
-    list_t *node;
-    int index;
-    char *p;
+	list_t *node;
+	int index;
+	char *p;
 
-    for (index = 0; index < 10; index++) {
-        node = nodes_start_with(info->alias, info->argv[0], '=');
-        if (!node) {
-            return 0;
-        }
+	for (index = 0; index < 10; index++)
+	{
+		node = nodes_start_with(info->alias, info->argv[0], '=');
+		if (!node)
+		{
+			return (0);
+		}
 
-        free(info->argv[0]);
-        p = _strchar(node->str, '=');
-        if (!p) {
-            return 0;
-        }
-        p = _strdupli(p + 1);
-        if (!p) {
-            return 0;
-        }
-        info->argv[0] = p;
-    }
-    return 1;
+		free(info->argv[0]);
+		p = _strchar(node->str, '=');
+		if (!p)
+			return (0);
+		p = _strdupli(p + 1);
+		if (!p)
+			return (0);
+		info->argv[0] = p;
+	}
+	return (1);
 }
 
 /**
@@ -102,33 +112,37 @@ int restore_alias(info_t *info)
  */
 int restore_vars(info_t *info)
 {
-    list_t *node;
-    int index = 0;
+	list_t *node;
+	int index = 0;
 
-    for (index = 0; info->argv[index]; index++) {
-        if (info->argv[index][0] != '$' || !info->argv[index][1]) {
-            continue;
-        }
+	for (index = 0; info->argv[index]; index++)
+	{
+		if (info->argv[index][0] != '$' || !info->argv[index][1])
+			continue;
 
-        if (!_strcmp(info->argv[index], "$?")) {
-            restore_string(&(info->argv[index]),
-                           _strdupli(convert_nums(info->status, 10, 0)));
-            continue;
-        }
-        if (!_strcmp(info->argv[index], "$$")) {
-            restore_string(&(info->argv[index]),
-                           _strdupli(convert_nums(getpid(), 10, 0)));
-            continue;
-        }
-        node = nodes_start_with(info->env, &info->argv[index][1], '=');
-        if (node) {
-            restore_string(&(info->argv[index]),
-                           _strdupli(_strcharr(node->str, '=') + 1));
-            continue;
-        }
-        restore_string(&info->argv[index], _strdupli(""));
-    }
-    return 0;
+		if (!_strcmp(info->argv[index], "$?"))
+		{
+			restore_string(&(info->argv[index]),
+					_strdupli(convert_nums(info->status, 10, 0)));
+			continue;
+		}
+		if (!_strcmp(info->argv[index], "$$"))
+		{
+			restore_string(&(info->argv[index]),
+					_strdupli(convert_nums(getpid(), 10, 0)));
+			continue;
+		}
+		node = nodes_start_with(info->env, &info->argv[index][1], '=');
+		if (node)
+		{
+			restore_string(&(info->argv[index]),
+					_strdupli(_strcharr(node->str, '=') + 1));
+			continue;
+		}
+		restore_string(&info->argv[index], _strdupli(""));
+
+	}
+	return (0);
 }
 
 /**
@@ -141,7 +155,7 @@ int restore_vars(info_t *info)
  */
 int restore_string(char **old, char *new)
 {
-    free(*old);
-    *old = new;
-    return 1;
+	free(*old);
+	*old = new;
+	return (1);
 }
