4080f80e94f84f750146f9017b89707f43fd3d9f
Mamba-cita <EDWINSJOHN52@gmail.com>
2023-12-12 08:28:31 +0300

    betty fix

--- memory.c
+++ memory.c
@@ -1,10 +1,8 @@
 #include "shell.h"
 
 /**
- * free_pointer - free the pointer memory and give it a NULL value
- *
- * @ptr: holds the address of the pointer to be freed
- *
+ * free_pointer - free the pointer memory and give it a NULL value.
+ * @ptr: holds the address of the pointer to be freed.
  * Return: 1(free), 0 otherwise.
  */
 int free_pointer(void **ptr)
@@ -14,6 +12,7 @@ int free_pointer(void **ptr)
         free(*ptr);
         *ptr = NULL;
         return 1;
-    }
+        }
     return 0;
 }
+
