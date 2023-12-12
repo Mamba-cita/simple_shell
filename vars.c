cbd8980ab6feb14d63b9159eaf0141ba665b739b
Mamba-cita <EDWINSJOHN52@gmail.com>
2023-12-12 15:06:15 +0300

    fix declaration

--- vars.c
+++ vars.c
@@ -8,33 +8,33 @@
  * @buf: the buffer for the character.
  * @p: position of the current address in the buffer.
  *
- * Return: 1( if chain delimeter), otherwise 0.
+ * Return: 1( if chain delimiter), otherwise 0.
  */
 int isa_chain(info_t *info, char *buf, size_t *p)
 {
-	size_t index = *p;
+    size_t index = *p;
 
-	if (buf[index] == '|' && buf[index + 1] == '|')
-	{
-		buf[index] = 0;
-		index++;
-		info->cmd_buf_type = CMD_OR;
-	}
-	else if (buf[index] == '&' && buf[index + 1] == '&')
-	{
-		buf[index] = 0;
-		index++;
-		info->cmd_buf_type = CMD_AND;
-	}
-	else if (buf[index] == ';')
-	{
-		buf[index] = 0;
-		info->cmd_buf_type = CMD_CHIAN;
-	}
-	else
-		return (0);
-	*p = index;
-	return (1);
+    if (buf[index] == '|' && buf[index + 1] == '|')
+    {
+        buf[index] = 0;
+        index++;
+        info->cmd_buf_type = CMD_OR;
+    }
+    else if (buf[index] == '&' && buf[index + 1] == '&')
+    {
+        buf[index] = 0;
+        index++;
+        info->cmd_buf_type = CMD_AND;
+    }
+    else if (buf[index] == ';')
+    {
+        buf[index] = 0;
+        info->cmd_buf_type = CMD_CHAIN;
+    }
+    else
+        return (0);
+    *p = index;
+    return (1);
 }
 
 /**
@@ -43,33 +43,29 @@ int isa_chain(info_t *info, char *buf, size_t *p)
  * @info: the structure parameter.
  * @buf: the buffer for character.
  * @p: position of the current address in the buffer.
- * @i: starting index in the buffer
+ * @index: starting index in the buffer
  * @len: length of the buffer
  *
  * Return: (void)
  */
 void find_chain(info_t *info, char *buf, size_t *p, size_t index, size_t len)
 {
-	size_t index = *p;
-
-	if (info->cmd_buf_type == CMD_AND)
-	{
-		if (info->status)
-		{
-			buf[i] = 0;
-			index = len;
-		}
-	}
-	if (info->cmd_buf_type == CMD_OR)
-	{
-		if (!info->status)
-		{
-			buf[i] = 0;
-			index = len;
-		}
-	}
-
-	*p = index;
+    if (info->cmd_buf_type == CMD_AND)
+    {
+        if (info->status)
+        {
+            buf[index] = 0;
+            *p = len;
+        }
+    }
+    if (info->cmd_buf_type == CMD_OR)
+    {
+        if (!info->status)
+        {
+            buf[index] = 0;
+            *p = len;
+        }
+    }
 }
 
 /**
@@ -81,26 +77,25 @@ void find_chain(info_t *info, char *buf, size_t *p, size_t index, size_t len)
  */
 int restore_alias(info_t *info)
 {
-	list_t *node;
-	int index;
-	char *p;
+    list_t *node;
+    char *p;
 
-	for (index = 0; index < 10; index++)
-	{
-		node = nodes_start_with(info->alias, info->argv[0], '=');
-		if (!node)
-		{
-			return (0);
-		}
+    for (int index = 0; index < 10; index++)
+    {
+        node = nodes_start_with(info->alias, info->argv[0], '=');
+        if (!node)
+        {
+            return (0);
+        }
 
-		free(info->argv[0]);
-		p = _strchar(node->str, '=');
-		if (!p)
-			return (0);
-		p = _strdupli(p + 1);
-		if (!p)
-			return (0);
-		info->argv[0] = p;
-	}
-	return (1);
+        free(info->argv[0]);
+        p = _strchar(node->str, '=');
+        if (!p)
+            return (0);
+        p = _strdupli(p + 1);
+        if (!p)
+            return (0);
+        info->argv[0] = p;
+    }
+    return (1);
 }
