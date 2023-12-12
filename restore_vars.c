bb82f12237b6b73284b5ae3bd7e7c656a058aa19
Mamba-cita <EDWINSJOHN52@gmail.com>
2023-12-12 15:18:47 +0300

    betty

--- restore_vars.c
+++ restore_vars.c
@@ -1,69 +1,60 @@
 #include "shell.h"
 
 /**
- * restore_vars - change the vars in tokenized string
- *
+ * restore_vars - change the vars in the tokenized string
  * @info: the structure parameter
  *
  * Return: 1 (if change), otherwise 0
  */
 int restore_vars(info_t *info)
 {
-	list_t *node;
-	int index = 0;
+    int index = 0;
 
-	for (index = 0; info->argv[index]; index++)
-	{
-		if (info->argv[index][0] != '$' || !info->argv[index][1])
-			continue;
+    for (index = 0; info->argv[index]; index++)
+    {
+        if (info->argv[index][0] != '$' || !info->argv[index][1])
+            continue;
 
-		if (!_strcmp(info->argv[index], "$?"))
-		{
-			restore_string(&(info->argv[index]),
-					_strdupli(convert_nums(info->status, 10, 0)));
-			continue;
-		}
-		if (!_strcmp(info->argv[index], "$$"))
-		{
-			restore_string(&(info->argv[index]),
-					_strdupli(convert_nums(getpid(), 10, 0)));
-			continue;
-		}
-		node = nodes_start_with(info->env, &info->argv[index][1], '=');
-		if (node)
-		{
-			restore_string(&(info->argv[index]),
-					_strdupli(_strcharr(node->str, '=') + 1));
-			continue;
-		}
-		restore_string(&info->argv[index], _strdupli(""));
-
-	}
-	return (0);
+        if (!_strcmp(info->argv[index], "$?"))
+        {
+            restore_string(&(info->argv[index]),
+                           _strdupli(convert_nums(info->status, 10, 0)));
+            continue;
+        }
+        if (!_strcmp(info->argv[index], "$$"))
+        {
+            restore_string(&(info->argv[index]),
+                           _strdupli(convert_nums(getpid(), 10, 0)));
+            continue;
+        }
+        list_t *node = nodes_start_with(info->env, &info->argv[index][1], '=');
+        if (node)
+        {
+            restore_string(&(info->argv[index]),
+                           _strdupli(_strcharr(node->str, '=') + 1));
+            continue;
+        }
+        restore_string(&info->argv[index], _strdupli(""));
+    }
+    return (0);
 }
 
 /**
  * restore_alias - rename an alias in the tokenized string
- *
  * @info: the structure parameter.
  *
  * Return: 1 (if rename), otherwise 0
  */
 int restore_alias(info_t *info)
 {
-    list_t *node;
-    char *p;
-
     for (int index = 0; index < 10; index++)
     {
-        node = nodes_start_with(info->alias, info->argv[0], '=');
+        list_t *node = nodes_start_with(info->alias, info->argv[0], '=');
         if (!node)
-        {
             return (0);
-        }
 
         free(info->argv[0]);
-        p = _strchar(node->str, '=');
+        char *p = _strchar(node->str, '=');
         if (!p)
             return (0);
         p = _strdupli(p + 1);
@@ -76,7 +67,6 @@ int restore_alias(info_t *info)
 
 /**
  * restore_string - changes/replaces the string
- *
  * @old: the old string address
  * @new: the new string
  *
@@ -84,7 +74,7 @@ int restore_alias(info_t *info)
  */
 int restore_string(char **old, char *new)
 {
-	free(*old);
-	*old = new;
-	return (1);
+    free(*old);
+    *old = new;
+    return (1);
 }
