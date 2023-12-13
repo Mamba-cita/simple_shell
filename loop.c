d32759dc925d312c0a1b13de9e224360aab9f426
Edwin's <94281852+Mamba-cita@users.noreply.github.com>
2023-12-13 02:10:37 +0000

    remove trailing whitespace

--- loop.c
+++ loop.c
@@ -48,9 +48,9 @@ int hsh(info_t *info, char **av)
  * @info: the parameter & return info struct
  *
  * Return: -1 if builtin not found,
- * 	0 if builtin executed successfully,
- * 	1 if builtin found but not successful,
- * 	2 if builtin signals exit()
+ * 0 if builtin executed successfully,
+ * 1 if builtin found but not successful,
+ * 2 if builtin signals exit()
  */
 int find_builtin(info_t *info)
 {
@@ -144,8 +144,7 @@ void fork_cmd(info_t *info)
 				exit(126);
 			exit(1);
 		}
-		
-	}
+		}
 	else
 	{
 		wait(&(info->status));
